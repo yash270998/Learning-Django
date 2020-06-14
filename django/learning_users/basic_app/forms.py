1956e97e20d757ecf3f82c0ae15a12528ba74b21
yash270998 <a00268810@student.ait.ie>
2020-06-14 18:01:52 +0100

    /Learning Users added --Login Feature + Registration

---
+++ django/learning_users/basic_app/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from basic_app.models import UserProfileInfo
+from django.contrib.auth.models import User
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta():
+        model = User
+        fields = ('username','email','password')
+
+
+class UserProfileInfoForm(forms.ModelForm):
+    # portfolio = forms.URLField(required=False)
+    # picture = forms.ImageField(required=False)
+
+    class Meta():
+        model = UserProfileInfo
+        fields = ('portfolio_site','profile_pic')
