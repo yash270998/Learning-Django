e342141f7222d317448ba989577b46953fd5b432
yash270998 <a00268810@student.ait.ie>
2020-06-16 13:31:19 +0100

    CRUD Operations

--- django/advcbv/basic_app/views.py
+++ django/advcbv/basic_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import View,TemplateView, ListView, DetailView
+from django.views.generic import (View,TemplateView, ListView, DetailView, CreateView, UpdateView, 
+                                    DeleteView)
 from . import models
+from django.urls import reverse_lazy
 # Create your views here.
 
 # def index(request):
@@ -23,3 +25,14 @@ class SchoolDetailView(DetailView):
     model = models.School
     template_name = 'basic_app/school_detail.html'
 
+class SchoolCreateView(CreateView):
+    fields = ('name','principal','location')
+    model = models.School
+
+class SchoolUpdateView(UpdateView):
+    fields = ('name','principal')
+    model = models.School
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy("basic_app:list")    
