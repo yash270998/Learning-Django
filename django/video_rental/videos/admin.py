52b7c2a749fb5d16ff95c3eb6ff1a5a356ae7989
yash270998 <a00268810@student.ait.ie>
2020-06-24 13:35:27 +0100

    Added filtr and search settings in admin.py

--- django/video_rental/videos/admin.py
+++ django/video_rental/videos/admin.py
@@ -3,5 +3,11 @@ from . import models
 # Register your models here.
 class MovieAdmin(admin.ModelAdmin):
     fields = ['release_year','title','length']
+
+    search_fields = ['title','release_year']
+
+    list_filter = ['release_year','title','length']
+    list_display = ['title','release_year','length']
+    list_editable = ['length']
 admin.site.register(models.Customer)
 admin.site.register(models.Movie,MovieAdmin)
